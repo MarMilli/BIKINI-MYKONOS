a156a9bed26f8b7fa55f4d1dc20d2d80df34db0f
MarMilli <marinemillionaire@gmail.com>
2018-12-14 17:40:10 +0700

    сделала слайдер и информционный блок

--- app/js/main.js
+++ app/js/main.js
@@ -1,100 +1,86 @@
 $(document).ready(function () {
-    $('.your-class').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<button class="slick__btnPrev" type="button"></button>',
-        nextArrow: '<button class="slick__btnNext" type="button"></button>',
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1189,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 625,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-        ]
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
     });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      vertical: true,
+      adaptiveHeight: true,
+      verticalSwiping: true,
+      arrows: false
+    });
+ $('.select_block').each(function (i) {
 
-    /*Анимация label в формах*/
+            var select = this;
 
-    function testLabelActive(field){
-        var label = field.parent('.formBlock').find(".label"),
-            valueField = field.val().length;
+            var select_field = $('.select_field', select);
+            var field_input = $('.field', select);
+            var fall_out = $('.select_fall_out', select);
+            var fall_out_element = $('span', fall_out);
+            var field_input_val = $('span:first-child', fall_out).html();
+            field_input.val(field_input_val);
 
-        if(valueField > 0)
-        {
-            label.addClass("label_active");
-        }
+
+            $(select).prev('label').click(function () {
+                select_field.click();
+            });
+
+            select_field.click(function() {
+                if (!fall_out.hasClass("open")) {
+                    openSelect();
+                } else {
+                    closeSelect();
+                }
+            });
+
+            function openSelect() {
+                closeSelect();
+                fall_out.animate({ height: "show" }, 200);
+                fall_out.addClass("open");
+                $(select).addClass("active");
+            }
+
+            fall_out_element.click(function () {
+                field_input_val = $(this).attr('data-rel');
+                if ( field_input_val == '0' ) {
+                    field_input_val = " ";
+                    field_input.val(field_input_val);
+                } else {
+                    setTimeout(function()
+                        {
+                            field_input.val(field_input_val);
+                        }, 200);
+                }
+                closeSelect();
+
+            });
+
+
+    });
+    
+
+
+
+function closeSelect() {
+    $("div.select_fall_out.open").animate({ height: "hide" }, 200).removeClass("open");
+    $('.select_block.active').removeClass("active");
+}
+
+$(document).click(function(event) {
+    if ( $(event.target).closest($('.select_block')).length || $(event.target).closest($('.select_block').prev('label')).length ) {} else {
+        closeSelect();
     }
-    function labelActive(field){
-        var label = field.parent('.formBlock').find(".label");
-        label.addClass("label_active");
-    }/*end function labelActive*/
-
-    function labelNoActive(field){
-        var label = field.parent('.formBlock').find(".label");
-        filling = field.val().length;
-
-        if(filling < 1)
-        {
-            label.removeClass("label_active");
-        }
-    }/*end function labelNoActive*/
-
-    $(".field").focusin(function(){
-        labelActive($(this));
-    });/*end $(".field").focusin*/
-
-    $(".field").focusout(function(){
-        labelNoActive($(this));
-    });/*end $(".field").focusout*/
-    $(".field").each(function(){
-        testLabelActive($(this));
-    });/*end $(".field").each*/
-
-    //функция плавной прокрутки страницы до якоря
-    var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 400);
-        return false;
-    });
-
-    var  toggle = false; // переключатель
-    //функция открытия выпадающего меню
-    function openMenuMainBurger(){
-        if (toggle == false)
-            $('.bgModalWindow').show(); //затемняем фон
-        else $('.bgModalWindow').hide();
-            $('.dropdownMenuButton').toggleClass('close'); //меняем иконку бургера на крестик
-            $('.menuMain').toggleClass('menuMainBurger'); // открываем меню
-            $('.menuMain>div').toggleClass('menuMain__items', toggle).toggleClass('menuMainBurger__items');
-            toggle = !toggle;
-        };
-    //вызов функции открытия выпадающего меню
-    $(function(){
-        $('.dropdownMenuButton').click(function(){
-            openMenuMainBurger();
-        });
-    });
-    //закрытие меню при клике на подложке и по пунктам меню
-    $('.bgModalWindow').click( function(){
-              openMenuMainBurger();
-    });
-    $('a[href*="#"]').click( function(){
-        if (toggle == true)
-              openMenuMainBurger();
-    });
 });
+
+});
+
+
